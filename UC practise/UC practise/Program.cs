40f5f1b08400be91913b4f6a309b61bf4cb17c21
Shwgupta@12 <guptashweta23797@gmail.com>
2022-06-10 20:36:47 +0845

    UC3 files update

--- UC practise/UC practise/Program.cs
+++ UC practise/UC practise/Program.cs
@@ -6,18 +6,24 @@ namespace UC_practise
     {
         static void Main(string[] args)
         {
-            int WagePerHR = 20;
+            int WagePerHr = 20;
             Random random = new Random();
-            int value = random.Next(0, 2);
+            int value = random.Next(0, 3);
             if (value == 0)
             {
                 Console.WriteLine("Employee is absent");
                 Console.WriteLine(value);
             }
+            else if (value == 1)
+            {
+                Console.WriteLine("Eployee is for part time");
+                int salary = WagePerHr * 4;
+                Console.WriteLine(salary);
+            }
             else
             {
-                Console.WriteLine("Employee is present");
-                int salary = WagePerHR * 8;
+                Console.WriteLine("Emplyoee is for full time");
+                int salary = WagePerHr * 8;
                 Console.WriteLine(salary);
             }
         }
